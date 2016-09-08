58f40ddd7ce0b37a9f200ba08c38f163faceae1e
Börcsök József <jozsef.borcsok@avon.com>
2016-09-08 14:09:42 +0200

    upgrade InfluxDB

--- influxdb/build.sh
+++ influxdb/build.sh
@@ -3,12 +3,21 @@
 set -e
 
 PREFIX=${DOCKER_PREFIX:-blackbelt}
-INFLUXDB_VERSION=${1:-0.13.0-1}
+INFLUXDB_VERSION=${1:-1.0.0}
 
 CWD="`dirname $0`"
 
+if [ "${INFLUXDB_VERSION}" > "0" ]
+then
+    cp "${CWD}/config-1.0.toml" "${CWD}/config.toml"
+else
+    cp "${CWD}/config-0.x.toml" "${CWD}/config.toml"
+fi
+
 docker build -t "${PREFIX}/influxdb" --build-arg "INFLUXDB_VERSION=${INFLUXDB_VERSION}" "${CWD}"
 docker tag  "${PREFIX}/influxdb" "${PREFIX}/influxdb:${INFLUXDB_VERSION}" 
+
+rm -f "${CWD}/config.toml"
 
 if [ "x1" == "x${PUSH_DOCKER_IMAGE}" ]
 then
