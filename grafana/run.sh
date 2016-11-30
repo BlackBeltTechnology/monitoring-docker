f66a2ace2f0f8ffb3c8fd7d562db5aa78b99fc00
Börcsök József <jozsef.borcsok@blackbelt.hu>
2016-11-30 18:52:49 +0100

    using volumes created previously

--- grafana/run.sh
+++ grafana/run.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 PREFIX=${DOCKER_PREFIX:-blackbelt}
 
-docker run -d --name monitoring-grafana -p 3000:3000 "${PREFIX}/grafana"
+# docker volume create grafana-config
+# docker volume create grafana-data
+# docker volume create grafana-log
+
+docker run -d --name grafana -v grafana-config:/opt/grafana/conf -v grafana-data:/var/lib/grafana -v grafana-log:/var/log/grafana -p 3000:3000 "${PREFIX}/grafana"
 
